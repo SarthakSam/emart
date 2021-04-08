97045707cecf4b56db1451e43d40da97229c248e
Sarthak <sarthak242@gmail.com>
2021-04-08 20:59:32 +0530

    Feat: added actions and login in reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,42 @@
 export const actions = {
-    INITIALIZE_PRODUCTS: 'INITIALIZE_PRODUCTS'
+    INITIALIZE_PRODUCTS: 'INITIALIZE_PRODUCTS',
+    ADD_TO_WISHLIST: 'ADD_TO_WISHLIST',
+    REMOVE_FROM_WISHLIST: 'REMOVE_FROM_WISHLIST',
+    ADD_TO_CART: 'ADD_TO_CART',
+    REMOVE_TO_CART: 'REMOVE_FROM_CART',
 }
 
 export class InitializeProducts {
     constructor(payload) {
         this.type = actions.INITIALIZE_PRODUCTS;
+        this.payload = payload;
+    }
+}
+
+export class AddToWishlist {
+    constructor(payload) {
+        this.type = actions.ADD_TO_WISHLIST;
+        this.payload = payload;
+    }
+}
+
+export class RemoveFromWishlist {
+    constructor(payload) {
+        this.type = actions.REMOVE_FROM_WISHLIST;
+        this.payload = payload;
+    }
+}
+
+export class AddToCart {
+    constructor(payload) {
+        this.type = actions.ADD_TO_CART;
+        this.payload = payload;
+    }
+}
+
+export class RemoveFromCart {
+    constructor(payload) {
+        this.type = actions.REMOVE_FROM_CART;
         this.payload = payload;
     }
 }
--- src/contexts/reducers/store.reducer.js
+++ src/contexts/reducers/store.reducer.js
@@ -3,12 +3,42 @@ import { actions } from '../../actions';
 export const initialState = {
     products: [],
     cart: [],
-    wishlisted: []
+    wishlist: []
 }
 
 export function reducer(state, action) {
     switch( action.type ) {
         case actions.INITIALIZE_PRODUCTS:    return { ...state, products: action.payload };
+        case actions.ADD_TO_WISHLIST:        return { ...state, wishlist: action.payload};
+        case actions.REMOVE_FROM_WISHLIST:   return removeFromWishlist(state, action);
+        case actions.ADD_TO_CART:            return addToCart(state, action);
+        case actions.REMOVE_FROM_CART:       return removeFromCart(state, action);
         default:                             return state;
     }
+}
+
+function removeFromWishlist(state, action) {
+    return { ...state, wishlist: state.wishlist.filter( product => product.id === action.payload.id ) };
+}
+
+function addToCart(state, action) {
+    const product = state.cart.filter( product => product.id === action.payload.id );
+    if( product ) {
+        const filteredCart = state.cart.filter( product => product.id !== action.payload.id);
+        return { ...state, cart: [...filteredCart, { ...product, quantity: product.quantity + 1 }]};
+    } else {
+        return { ...state, cart: [...state.cart, { ...action.payload, quantity: 1  }] };
+    }
+}
+
+function removeFromCart(state, action) {
+    const product = state.cart.filter( product => product.id === action.payload.id );
+    const filteredCart = state.cart.filter( product => product.id !== action.payload.id);
+    if( product.quantity == 1 ) {
+        return { ...state, cart: filteredCart};
+    }
+    else {
+        return { ...state, cart: filteredCart.push({ ...product, quantity: product.quantity - 1 })};
+    }
+
 }
